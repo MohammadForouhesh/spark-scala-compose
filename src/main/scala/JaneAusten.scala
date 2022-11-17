1088196c3a08c5f6e55b7aa9bc31bdc83040f08a
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-11-18 01:27:27 +0330

    Update JaneAusten.scala

--- src/main/scala/JaneAusten.scala
+++ src/main/scala/JaneAusten.scala
@@ -1,11 +1,8 @@
 package word_count
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{SparkSession, SQLContext, DataFrame}
 import org.apache.spark.rdd.RDD
-
 import scala.util.matching.Regex
 
 case class Jane(word: String)
@@ -13,18 +10,22 @@ case class Jane(word: String)
 object Jane {
     val conf: SparkConf = new SparkConf().setMaster("spark://127.0.0.1:7077").setAppName("word count")
     val sc: SparkContext = new SparkContext(conf)
-    val janeRdd: RDD[Jane] = sc.textFile("C:\\Users\\snapp\\Documents\\Courses\\spark-scala\\resources\\JaneAusten.txt")
+    val janeRdd: RDD[(Int, Jane)] = sc.textFile("C:\\Users\\snapp\\Documents\\Courses\\spark-scala\\resources\\JaneAusten.txt")
                             .flatMap(_.split(" "))
                             .map(w => preprocess(w))
-                            .map(e => Jane(e)).persist()
+                            .map(e => Jane(e))
+                            .map(w => (w, 1))
+                            .reduceByKey(_ + _)
+                            .map(p => (p._2, p._1))
+                            .sortByKey(false)
 
     def preprocess(word: String): String = {
-        val pattern = new Regex("[a-zA-Z0-9\\s]")
-        (pattern findAllIn word).mkString("")
+        val pattern = new Regex("[^a-zA-Z0-9\\s]")
+        (pattern.replaceAllIn(word, " ")).mkString("")
     }
 
     def count_words = {
-        janeRdd.toDF().groupBy(identity).count()//.sort(desc("count")).show()
+        janeRdd.collect()
     }
 }
 
