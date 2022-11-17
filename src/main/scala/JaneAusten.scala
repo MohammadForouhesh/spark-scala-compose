778514bfb5b62aa8cf4574b854c00de7c0202999
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-11-17 18:40:05 +0330

    Create JaneAusten.scala

---
+++ src/main/scala/JaneAusten.scala
@@ -0,0 +1,34 @@
+package word_count
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.rdd.RDD
+
+import scala.util.matching.Regex
+
+case class Jane(words: String)
+
+object Jane {
+    val conf: SparkConf = new SparkConf().setMaster("spark://1fe76fc30194:7077").setAppName("word count")
+    val sc: SparkContext = new SparkContext(conf)
+    val janeRdd: RDD[Jane] = sc.textFile("/resources/resources/JaneAusten.txt")
+                            .flatMap(_.split(" "))
+                            .map(w => preprocess(w))
+                            .map(e => Jane(e)).persist()
+
+    def preprocess(word: String): String = {
+        val pattern = new Regex("[a-zA-Z0-9\\s")
+        (pattern findAllIn word).mkString("")
+    }
+
+    def count_words = {
+        janeRdd.count()//.sort(desc("count")).show()
+    }
+}
+
+object Main {
+    def main(args: Array[String]): Unit = {
+        println(Jane.count_words)
+    }
+}
