20e364dbd02de87f51139ce6cecbbd345e5ebe61
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-11-20 01:22:05 +0330

    Update JaneAusten.scala

--- src/main/scala/JaneAusten.scala
+++ src/main/scala/JaneAusten.scala
@@ -1,36 +1,18 @@
 package word_count
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{SparkSession, SQLContext, DataFrame}
+import org.apache.spark.sql.{SQLContext, DataFrame}
 import scala.util.matching.Regex
-import org.apache.log4j.{Logger, Level}
 import org.apache.spark.sql.functions.desc
 import org.apache.spark.ml.feature.StopWordsRemover
 
 
 case class Jane(word: String)
 
-object Spark {
-    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
-    System.setProperty("hadoop.home.dir", "C:\\hadoop\\")
-    private val conf: SparkConf = new SparkConf()
-        .setMaster("local[2]")
-        .setAppName("word count")
-        .set("spark.driver.allowMultipleContexts", "false")
-
-    val spark: SparkSession = SparkSession
-        .builder()
-        .config(conf)
-        .getOrCreate()
-    val sqlContext: SQLContext = spark.sqlContext
-    import sqlContext.implicits._
-}
-
 object JaneAusten {
     lazy val stopWords = StopWordsRemover.loadDefaultStopWords("english")
+    lazy val pattern = new Regex("[^a-zA-Z0-9]")
     
     def notPunctuation(word: String): Array[String] = {
-        lazy val pattern = new Regex("[^a-zA-Z0-9]")
         (pattern.replaceAllIn(word, ",")).mkString("").split(",")
     }
 
@@ -39,9 +21,9 @@ object JaneAusten {
     }
 
     def count_words = {
-        val sqlContext: SQLContext = Spark.spark.sqlContext
+        val sqlContext: SQLContext = SparkClient.spark.sqlContext
         import sqlContext.implicits._
-        Spark.spark.read.text("resources/JaneAusten.txt")
+        SparkClient.spark.read.text("resources/JaneAusten.txt")
                 .flatMap(_.toString.split(" "))
                 .flatMap(notPunctuation)
                 .map(_.toLowerCase)
@@ -56,6 +38,6 @@ object JaneAusten {
 object Main {
     def main(args: Array[String]): Unit = {
         JaneAusten.count_words.show()
-        Spark.spark.close()
+        SparkClient.spark.close()
     }
 }
