82cb859199e4f358d67eb1fc372629e79bfb8d1d
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-11-22 01:39:11 +0330

    Update Clustering.scala

--- src/main/scala/Clustering.scala
+++ src/main/scala/Clustering.scala
@@ -1,8 +1,25 @@
 package SparkScala
 
-import org.apache.spark.ml.clustering.{KMeans, BisectingKMeans}
+import org.apache.spark.mllib.clustering.{KMeans, BisectingKMeans}
+import org.apache.spark.sql.{SQLContext, Column}
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.sql.functions.monotonically_increasing_id
 
 object Clustering {
+    lazy val numIterations: Int = 100
+    import SparkClient.sqlContext.implicits._
+    def compute(path: String) = {
+        val data = SparkClient.sc.textFile(f"resources/clustering/$path.txt")
+                             .map(_.trim)
+                             .map(_.split("    "))
+                             .map(s => Vectors.dense(s.map(_.toDouble)))
+                             .cache()
 
+        (2 to 25).map(KMeans.train(data, _, numIterations).computeCost(data))
+                .toDF()
+                .withColumn("numClusters", monotonically_increasing_id + 2)
+                .coalesce(1)
+                .write
+                .csv(f"results/kmeans_cost_$path")
+    }
 }
