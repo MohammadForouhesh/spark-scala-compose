fdb60c29f3ddba35c0b6073463ebff737f5b8b2f
Mohammad Forouhesh <mohammadh.forouhesh@gmail.com>
2022-11-23 00:13:35 +0330

    plotting with grids

--- src/main/python/plotting.py
+++ src/main/python/plotting.py
@@ -6,12 +6,14 @@ import os
 def plot_elbow(path: str):
     frame: pd.DataFrame = pd.read_csv(f"{path}/{next(filter(lambda item: item.endswith('.csv'), os.listdir(path)))}")
     os.makedirs(f"plots/{path[: path.find('/')]}", exist_ok=True)
+    plt.grid(True)
     plt.plot(frame.numClusters, frame.value, '-*', label='kmeans-cost')
     plt.title(f"{path[path.find('/') + 1:].replace('_', ' ')} cost")
     plt.xlabel('Number of clusters')
     plt.ylabel('Silhouette metric')
     plt.legend()
     plt.savefig(f'plots/{path}.png')
+    plt.clf()
 
 
 if __name__ == '__main__':
